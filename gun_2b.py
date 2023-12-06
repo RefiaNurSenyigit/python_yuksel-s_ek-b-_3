bd5a9dcf4119965cb68b5ee35eb19e404a942e6c
Refia Nur Şenyiğit <refsen00@gmail.com>
2023-12-06 15:19:40 +0300

    Create gun_2b.py

---
+++ gun_2b.py
@@ -0,0 +1,7 @@
+frst=float(input("İlk sayıyı giriniz: "))
+scnd=float(input("İkinci sayıyı giriniz: "))
+toplam=frst+scnd
+fark=frst-scnd
+carpim=frst*scnd
+print("Toplam:",toplam,"\nFark:",fark,"\nÇarpım:",carpim)
+input()
