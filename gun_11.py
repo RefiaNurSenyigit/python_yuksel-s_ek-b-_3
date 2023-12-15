e590c313079875ec9a79c21e4019c45f7d5fa9f9
Refia Nur Şenyiğit <refsen00@gmail.com>
2023-12-16 00:05:25 +0300

    Create gun_11.py

---
+++ gun_11.py
@@ -0,0 +1,7 @@
+def nameer(first,last):
+    sum=first+" "+last
+    return sum
+
+first_name=input("Adınızı giriniz: ")
+last_name=input("Soyadınızı giriniz: ")
+print("Hello "+nameer(first_name,last_name)+"! You just delved into python")
